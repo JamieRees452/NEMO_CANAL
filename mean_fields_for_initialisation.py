8b7fbd897e14c7abc697438b138d56d0d410dfcf
Jamie Rees <jamierees60@googlemail.com>
2022-09-02 15:10:38 +0100

    Add files via upload

--- mean_fields_for_initialisation.py
+++ mean_fields_for_initialisation.py
@@ -10,8 +10,8 @@ import xarray            as xr
 MY_CANAL_directory = f'/home/users/jme22rs/NEMO/NEMO_CANAL/NEMO/tests/MY_CANAL'
 my_group_workspace = f'/gws/nopw/j04/oxford_es/jrees'
 
-latitude = np.loadtxt(f'{MY_CANAL_directory}/mean_fields/latitude.txt')
-depth    = np.loadtxt(f'{MY_CANAL_directory}/mean_fields/depth.txt')
+latitude = np.loadtxt(f'{MY_CANAL_directory}/mean_fields/latitude_nonuniform.txt')
+depth    = np.loadtxt(f'{MY_CANAL_directory}/mean_fields/depth_nonuniform.txt')
 
 # -------------------------------------------------------------------------------------------------------
 # Zonally and temporally averaged mean fields from the coupled model 
@@ -24,30 +24,25 @@ U_dataset = xr.open_mfdataset(U_infile).rename({'time_counter':'time','depthu':'
 T_dataset = xr.open_mfdataset(T_infile).rename({'time_counter':'time','deptht':'depth'})
 
 uoce = U_dataset.uo.sel(x=slice(xW25,xE25),y=slice(yS25,yN25),depth=slice(depthT25,depthB25))
-toce = T_dataset.thetao.sel(x=slice(xW25,xE25),y=slice(yS25,yN25),depth=slice(depthT25,depthB25))
-soce = T_dataset.so.sel(x=slice(xW25,xE25),y=slice(yS25,yN25),depth=slice(depthT25,depthB25))     
+toce = T_dataset.thetao.sel(x=slice(xW25,xE25),y=slice(yS25,yN25),depth=slice(depthT25,depthB25)) 
 
 umean = uoce.groupby('time.month').mean(dim='time').mean(dim='x')
 tmean = toce.groupby('time.month').mean(dim='time').mean(dim='x')
-smean = soce.groupby('time.month').mean(dim='time').mean(dim='x')
 
 umean  = umean.isel(month=slice(5, 7)).mean(dim='month')
 tmean  = tmean.isel(month=slice(5, 7)).mean(dim='month')
-smean  = smean.isel(month=slice(5, 7)).mean(dim='month')
 
 # -------------------------------------------------------------------------------------------------------
 # From text files
 # -------------------------------------------------------------------------------------------------------
 
 # If loading the text data
-#umean = np.loadtxt(f'/home/users/jme22rs/NEMO/NEMO_CANAL/NEMO/tests/MY_CANAL/mean_fields/umean.txt'); umean = np.reshape(umean, (47, 123))
-#tmean = np.loadtxt(f'/home/users/jme22rs/NEMO/NEMO_CANAL/NEMO/tests/MY_CANAL/mean_fields/tmean.txt'); tmean = np.reshape(tmean, (47, 123))
-#smean = np.loadtxt(f'/home/users/jme22rs/NEMO/NEMO_CANAL/NEMO/tests/MY_CANAL/mean_fields/smean.txt'); smean = np.reshape(smean, (47, 123))
+#umean = np.loadtxt(f'{MY_CANAL_directory}/mean_fields/umean.txt'); umean = np.reshape(umean, (47, 123))
+#tmean = np.loadtxt(f'{MY_CANAL_directory}/mean_fields/tmean.txt'); tmean = np.reshape(tmean, (47, 123))
 
 # For interpolation, it is easier to relabel the dimensions as follows
 umean_new = xr.DataArray(umean, coords={'depth': depth, 'latitude': latitude}, dims=['depth', 'latitude'])
 tmean_new = xr.DataArray(tmean, coords={'depth': depth, 'latitude': latitude}, dims=['depth', 'latitude'])
-smean_new = xr.DataArray(smean, coords={'depth': depth, 'latitude': latitude}, dims=['depth', 'latitude'])
 
 # ------------------------------------------------------------------------------------------------------------
 # Interpolate onto the CANAL grid
@@ -63,7 +58,6 @@ depth_canal = np.linspace(0, rn_domszz, z_points)
 # Interpolate onto the uniform CANAL grid
 umean_canal = umean_new.interp(latitude=lat_canal, depth=depth_canal, kwargs={"fill_value": "extrapolate"})
 tmean_canal = tmean_new.interp(latitude=lat_canal, depth=depth_canal, kwargs={"fill_value": "extrapolate"})
-smean_canal = smean_new.interp(latitude=lat_canal, depth=depth_canal, kwargs={"fill_value": "extrapolate"})
 
 # -----------------------------------------------------------------------------------------------------------
 # Broadcast into longitude
@@ -75,7 +69,17 @@ lon = xr.DataArray(np.ones(x_points), [("longitude", lon_canal)])
 # Broadcast arrays to repeat (similar to np.tile)
 umean_canal = lon*umean_canal 
 tmean_canal = lon*tmean_canal 
-smean_canal = lon*smean_canal 
+
+umean_canal = umean_canal.transpose("depth", "latitude", "longitude")
+tmean_canal = tmean_canal.transpose("depth", "latitude", "longitude")
+
+vmean_canal = xr.zeros_like(umean_canal)
+smean_canal = 35*xr.ones_like(tmean_canal)
+
+umean_canal = xr.DataArray(umean_canal, name='zonal_velocity')
+vmean_canal = xr.DataArray(vmean_canal, name='meridional_velocity')
+tmean_canal = xr.DataArray(tmean_canal, name='temperature')
+smean_canal = xr.DataArray(smean_canal, name='salinity')
 
 # -----------------------------------------------------------------------------------------------------------
 # Save to netCDF
@@ -83,10 +87,6 @@ smean_canal = lon*smean_canal
 
 # Save data to netCDF
 umean_canal.to_netcdf(f'{MY_CANAL_directory}/EXP00/umean_canal.nc')
+umean_canal.to_netcdf(f'{MY_CANAL_directory}/EXP00/vmean_canal.nc')
 tmean_canal.to_netcdf(f'{MY_CANAL_directory}/EXP00/tmean_canal.nc')
 smean_canal.to_netcdf(f'{MY_CANAL_directory}/EXP00/smean_canal.nc')
-
-# When loading the data, we select the  named data variable
-#umean_canal = xr.open_dataset(f'{MY_CANAL_directory}/EXP00/umean_canal.nc').__xarray_dataarray_variable__
-#tmean_canal = xr.open_dataset(f'{MY_CANAL_directory}/EXP00/tmean_canal.nc').__xarray_dataarray_variable__
-#smean_canal = xr.open_dataset(f'{MY_CANAL_directory}/EXP00/smean_canal.nc').__xarray_dataarray_variable__
